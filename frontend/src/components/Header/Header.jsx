bb1666f362e69b5226a4712c0e918016ee21e28d
Roubin Raj <151029462+raaz-singh@users.noreply.github.com>
2025-05-29 10:22:21 +0530

    New1

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <div className="header-contents">
+                <h2>Order your favourite food here</h2>
+                <p>Craving your favorite meals without leaving the comfort of your home? Our food delivery service brings delicious dishes from top local restaurants straight to your doorstep—fast, fresh, and hassle-free. Whether you're in the mood for comforting classics, international flavors, or healthy options, we've got something for every taste. With easy online ordering, real-time tracking, and secure payment options, satisfying your hunger has never been more convenient. Sit back, relax, and let us deliver happiness—one bite at a time.</p>
+                <button>View Menu</button>
+            </div>
+        </div>
+    )
+}
+
+export default Header
