b9c71decd6b768275f41a698e6ead6d8e1378063
Roubin Raj <151029462+raaz-singh@users.noreply.github.com>
2025-06-12 07:18:22 +0530

    new5

--- frontend/src/pages/PlaceOrder/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -3,29 +3,33 @@ import './PlaceOrder.css'
 import { StoreContext } from '../../context/StoreContext'
 
 const PlaceOrder = () => {
-
-    const {getTotalCartAmount} = useContext(StoreContext)
+    const { getTotalCartAmount } = useContext(StoreContext)
 
     return (
         <form className='place-order'>
             <div className='place-order-left'>
                 <p className="title">Delivery Information</p>
+
                 <div className="multi-fields">
                     <input type="text" placeholder='First name' />
                     <input type="text" placeholder='Last name' />
                 </div>
+
                 <input type="email" placeholder='Email' />
                 <input type="text" placeholder='Street' />
-            </div>
-            <div className="multi-fields">
+
+                <div className="multi-fields">
                     <input type="text" placeholder='City' />
                     <input type="text" placeholder='State' />
                 </div>
+
                 <div className="multi-fields">
                     <input type="text" placeholder='Zip Code' />
                     <input type="text" placeholder='Country' />
                 </div>
+
                 <input type="text" placeholder='Phone' />
+            </div>
 
             <div className="place-order-right">
                 <div className="cart-total">
@@ -38,12 +42,12 @@ const PlaceOrder = () => {
                         <hr />
                         <div className="cart-total-details">
                             <p>Delivery Fee</p>
-                            <p>${getTotalCartAmount()===0?0:2}</p>
+                            <p>${getTotalCartAmount() === 0 ? 0 : 2}</p>
                         </div>
                         <hr />
                         <div className="cart-total-details">
                             <b>Total</b>
-                            <b>${getTotalCartAmount()===0?0:getTotalCartAmount()+2}</b>
+                            <b>${getTotalCartAmount() === 0 ? 0 : getTotalCartAmount() + 2}</b>
                         </div>
                     </div>
                     <button>Proceed to Payment</button>
